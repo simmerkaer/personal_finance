d2a2ac9cf86b8a1a2a7d55d2768456467eb4e595
esow <simmerkaer@gmail.com>
2018-11-26 19:01:59 +0100

    created basic layout

--- frontend/personal_finance/src/App.tsx
+++ frontend/personal_finance/src/App.tsx
@@ -1,26 +1,17 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Layout from "./components/TopNavigation/Layout";
 
 class App extends Component {
-  render() {
+  public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <CssBaseline />
+        <div className="App">
+          <Layout />
+        </div>
+      </>
     );
   }
 }
